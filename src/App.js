9d7f9aa3fc4ddeacc0ec9641320df1ae73f9e6aa
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-14 23:06:02 +0600

    we are ready to go

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='flex justify-center items-center min-h-screen'>
+      <div className='max-w-screen-lg	mx-auto rounded-2xl border drop-shadow p-5'>
+        <h2>hello world!</h2>
+        <Routes>
+          {/* <Route path ="/" element = {}/> */}
+        </Routes>
+      </div>
+    </main>
   );
 }
 
